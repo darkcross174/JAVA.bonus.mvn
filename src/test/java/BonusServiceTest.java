d04cdc8f53ab715030dd157eaf9bc9775ed93f2d
Alexey Andreev <dark-cross@rambler.ru>
2020-11-25 00:27:54 +0300

    'Bonus.mvn'

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void houldCalculateRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void houldCalculateRegisteredOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void houldCalculateNonRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+
+    }
+    @Test
+    void houldCalculateNonRegisteredOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+}
+
